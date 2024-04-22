6fbaf1dac5ff8b2e98ed3b2a7bc606466dbe7546
Louis Bove <louisbove84@gmail.com>
2024-04-22 20:49:25 +0000

    changing embedding method

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,10 +1,15 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
-
+from langchain_openai import OpenAIEmbeddings
+import os
 
 def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
+    # embeddings = BedrockEmbeddings(
+    #     credentials_profile_name="default", region_name="us-east-1"
+    # )
     # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+    embeddings = OpenAIEmbeddings(api_key=os.environ["OPENAI_API_KEY"])
+    # embeddings = embeddings_model.embed_documents(
+
     return embeddings
